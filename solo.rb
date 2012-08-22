1e4b0108a6341ba7b28e1d4063178edbf726270b
Carl-Johan Blomqvist <carl@cjblomqvist.com>
2012-08-22 20:31:01 +0200

    Fix bug

--- solo.rb
+++ solo.rb
@@ -1,3 +1,4 @@
 file_cache_path "/var/chef-solo"
 cookbook_path "/var/chef-solo/cookbooks"
-data_bag_path "/var/chef-solo/data_bags"
+# Yes, definitely not optimal :D
+data_bag_path "/var/chef-solo/cookbooks/data_bags"

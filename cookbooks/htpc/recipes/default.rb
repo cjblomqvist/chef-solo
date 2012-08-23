f20f9bdda0e8bda7bf0d4f57c78ecba0e01c2f2a
Carl-Johan Blomqvist <carl@cjblomqvist.com>
2012-08-23 16:32:14 +0200

    Actually, mongodb cookbook had a separate recipe for adding repo to apt

--- cookbooks/htpc/recipes/default.rb
+++ cookbooks/htpc/recipes/default.rb
@@ -45,14 +45,3 @@ ppa "chris-lea/node.js"
 package "nodejs"
 package "npm"
 
-# ==============================================================
-# Add package repo for mongodb (seems like the mongodb cookbook doesn't handle this itself)
-# ==============================================================
-apt_repository "mongodb" do
-  uri "http://downloads-distro.mongodb.org/repo/ubuntu-upstart"
-  distribution "dist"
-  components ["10gen"]
-  keyserver "keyserver.ubuntu.com"
-  key "7F0CEB10"
-  action :add
-end
